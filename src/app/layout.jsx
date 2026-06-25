5b4d4c539cc3dea0ddffb838570dc77c3f6936fa
melitacruces <melitacruces@gmail.com>
2026-06-24 20:42:32 -0400

    perf: optimize font loading to prevent FOUT

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -30,6 +30,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
+        <link rel="preload" as="style" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
